7280538314916d6677e61439702334ea2065cffe
maciejmar <memgos@gmail.com>
2026-03-18 08:20:43 +0100

    new tenant

--- hub-frontend/src/app/environment.ts
+++ hub-frontend/src/app/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
   apiBaseUrl: 'http://localhost:18000/api/v1',
   oidc: {
-    issuer: 'https://login.microsoftonline.com/3cf0f665-a664-4ad7-a75f-520538df5523/v2.0',
+    issuer: 'https://login.microsoftonline.com/29bb5b9c-200a-4906-89ef-c651c86ab301/v2.0',
     clientId: 'e517ed53-e3c5-42b4-b8c9-e35974e369ee',
     scope: 'openid profile email',
     redirectPath: '/index.html',
