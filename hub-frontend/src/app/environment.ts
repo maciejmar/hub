f342a807be8706bd4176aef736b252dbb261567c
maciejmar <memgos@gmail.com>
2026-03-18 08:54:59 +0100

    fix apiBaseUrl for azure

--- hub-frontend/src/app/environment.ts
+++ hub-frontend/src/app/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'http://localhost:18000/api/v1',
+  apiBaseUrl: 'https://ai-apps-portal-test.northeurope.cloudapp.azure.com:18000/api/v1',
   oidc: {
     issuer: 'https://login.microsoftonline.com/29bb5b9c-200a-4906-89ef-c651c86ab301/v2.0',
     clientId: 'e517ed53-e3c5-42b4-b8c9-e35974e369ee',
