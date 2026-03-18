e68c54e1578507189e57ec00068e847c99699527
maciejmar <memgos@gmail.com>
2026-03-18 07:47:12 +0100

    new client ID, redirect to index, CORS for azure

--- hub-frontend/src/app/environment.ts
+++ hub-frontend/src/app/environment.ts
@@ -3,8 +3,8 @@ export const environment = {
   apiBaseUrl: 'http://localhost:18000/api/v1',
   oidc: {
     issuer: 'https://login.microsoftonline.com/3cf0f665-a664-4ad7-a75f-520538df5523/v2.0',
-    clientId: '4fe1d350-b8b4-4c0b-988d-0323acdf8175',
-    scope: 'openid profile email api://4fe1d350-b8b4-4c0b-988d-0323acdf8175/access_as_user',
-    redirectPath: '/auth/callback',
+    clientId: 'e517ed53-e3c5-42b4-b8c9-e35974e369ee',
+    scope: 'openid profile email',
+    redirectPath: '/index.html',
   },
 };
