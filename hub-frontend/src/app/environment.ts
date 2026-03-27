58cf18ebaf4638eba4a9c48398f26ca513899f8d
maciejmar <memgos@gmail.com>
2026-03-27 06:22:31 +0100

    dev2 changes for server

--- hub-frontend/src/app/environment.ts
+++ hub-frontend/src/app/environment.ts
@@ -1,10 +1,12 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'https://ai-apps-portal-test.northeurope.cloudapp.azure.com/api/v1',
+  // apiBaseUrl: 'https://ai-apps-portal-test.northeurope.cloudapp.azure.com/api/v1', // stara domena Azure VM
+  apiBaseUrl: 'https://10.112.32.19/portal/api/v1',
   oidc: {
     issuer: 'https://login.microsoftonline.com/29bb5b9c-200a-4906-89ef-c651c86ab301/v2.0',
     clientId: 'e517ed53-e3c5-42b4-b8c9-e35974e369ee',
     scope: 'openid profile email',
-    redirectPath: '/index.html',
+    // redirectPath: '/index.html', // stara domena Azure VM
+    redirectPath: '/portal/index.html',
   },
 };
